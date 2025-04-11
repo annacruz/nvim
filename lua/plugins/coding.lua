f23c58aca91f7407e6dd7b60dfccd247ec497176
Anna Cruz <anna.cruz@gmail.com>
2025-04-11 14:40:17 +0200

    Create configuration for coding plugins

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,41 @@
+return {
+  --Create annotations with one keybind, and jump your cursor in the inserted annotation
+  {
+    "danymat/neogen",
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen Comment",
+      },
+    },
+    opts = { snippet_engine = "luasnip" },
+  },
+
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+
+  -- Refactoring tool
+  {
+    "ThePrimagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
+  },
+}
