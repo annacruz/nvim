546435789d948fb1ca1c2cad3a57419f57dff571
Anna Cruz <anna.cruz@gmail.com>
2025-04-11 14:08:20 +0200

    Config keymaps and discipline based in @craftzdog config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,55 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local discipline = require("yui.discipline")
+discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "C-x")
+
+-- Delete a word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m", "<C-i>", opts)
+
+-- Tabs management
+keymap.set("n", "te", ":tabedit<Return>", opts) -- New tab
+keymap.set("n", "<tab>", ":tabnext<Return>", opts) -- Go to next tab
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts) -- Go to prev tab
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts) -- horizontal
+keymap.set("n", "sv", ":vsplit<Return>", opts) -- vertical
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto({ next = true })
+end, opts)
+
+-- Navbuddy
+keymap.set("n", "nb", ":Navbuddy<Return>", opts)
+
+-- Tmux navigator
+keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
